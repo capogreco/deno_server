208619fb7a68b93fcc775e425a7248d38f3b003c
capogreco <thomas.capogreco@gmail.com>
2022-10-05 09:43:24 +1100

    squares in conversation

--- server.js
+++ server.js
@@ -1,19 +1,59 @@
-import { serve } from "https://deno.land/std@0.157.0/http/server.ts"
-import { serveDir } from "https://deno.land/std@0.157.0/http/file_server.ts"
+import { serve } from "https://deno.land/std@0.158.0/http/server.ts"
+import { serveDir } from "https://deno.land/std@0.158.0/http/file_server.ts"
+import { getNetworkAddr } from "https://deno.land/x/local_ip/mod.ts"
+
+const local_ip = await getNetworkAddr()
+console.log (`local area network IP: ${ local_ip }`)
 
 serve (handler, { port: 80 })
 
-function handler (req) {
+let sockets = []
+
+function handler (incoming_req) {
+
+    let req = incoming_req
+
+    const upgrade = req.headers.get ("upgrade") || ""
+
+    if (upgrade.toLowerCase() == "websocket") {
+
+        const { socket, response } = Deno.upgradeWebSocket (req)
+
+        socket.onopen  = () => {
+            console.log (`server WebSocket opened`)
+
+            // add the socket to the sockets array
+            sockets.push (socket)
+        }
+
+        socket.onclose = () => {
+            console.log (`server WebSocket closed`)
+
+            // filters closed sockets (ie. sockets without
+            // a .readyState of 1) out of the array
+            sockets = sockets.filter (s => s.readyState == 1)
+        }
+
+        socket.onerror = e  => console.dir (e)
+
+        socket.onmessage = e => {
+            console.log (`incoming message: ${ e.data }`)
+
+            // send the message data back out 
+            // to each of the sockets in the array
+            sockets.forEach (s => s.send (e.data))
+        }
+
+        return response
+    }
+
+    if (req.url.endsWith (`/`)) {
+        req = new Request (`${ req.url }index.html`, req)
+    }
 
     const options = {
         fsRoot: `public`
     }
-
-    if (req.url.endsWith (`/`)) {
-        const aug_req = new Request (`${ req.url }index.html`, req)
-        return serveDir (aug_req, options)
-    }
-
 
     return serveDir (req, options)
 }
